e37a94eb06571c5e1ce9b3c2665c49f2028fdeff
chnic888 <chnic888@gmail.com>
2017-07-06 12:34:40 +0800

    Add goodbye service

---
+++ EurekaGoodbyeServiceProvider/src/main/java/com/chnic/service/provider/controller/GoodbyeController.java
@@ -0,0 +1,39 @@
+package com.chnic.service.provider.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodbyeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(GoodbyeController.class);
+	
+	@Autowired
+	private Registration registration;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@SuppressWarnings("deprecation")
+	@GetMapping(value = "/goodbye")
+	public String sayHello(@RequestParam(name = "title") String title, @RequestParam(name = "name") String name) {
+		List<ServiceInstance> instanceList = discoveryClient.getInstances(registration.getServiceId());
+		logger.info("Instance Size: " + instanceList.size());
+		
+		ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+		logger.info("host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", service_id:" + registration.getServiceId());
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Goodbye ").append(title).append(", ").append(name);
+		return sb.toString();
+	}
+}
---
+++ EurekaGoodbyeServiceProvider/src/test/java/com/chnic/service/provider/EurekaGoodbyeServiceProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chnic.service.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaGoodbyeServiceProviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
