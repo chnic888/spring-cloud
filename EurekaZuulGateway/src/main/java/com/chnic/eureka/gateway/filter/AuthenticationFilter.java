d6c91d5e93286ac2723879512914047971883094
chnic888 <chnic888@gmail.com>
2017-07-07 15:37:27 +0800

    Add refresh scope

--- EurekaZuulGateway/src/main/java/com/chnic/eureka/gateway/filter/AuthenticationFilter.java
+++ EurekaZuulGateway/src/main/java/com/chnic/eureka/gateway/filter/AuthenticationFilter.java
@@ -11,33 +11,33 @@ import com.netflix.zuul.context.RequestContext;
 
 public class AuthenticationFilter extends ZuulFilter {
 	private static Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
-	
+
 	private final String TOKEN = "12345";
-	
+
 	@Override
 	public Object run() {
 		RequestContext context = RequestContext.getCurrentContext();
-        HttpServletRequest request = context.getRequest();
-		
-        String token = request.getHeader("Authorization");
-        if (!TOKEN.equalsIgnoreCase(token)) {
-        	logger.error("Authencated failed with token: " + token);
-        	context.setSendZuulResponse(false);
-        	context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
-        	context.setResponseBody("Unauthorized Request");
-        	return null;
-        }
-        
-        return null;
+		HttpServletRequest request = context.getRequest();
+
+		String token = request.getHeader("Authorization");
+		if (!TOKEN.equalsIgnoreCase(token)) {
+			logger.error("Authencated failed with token: " + token);
+			context.setSendZuulResponse(false);
+			context.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+			context.setResponseBody("Unauthorized Request");
+			return null;
+		}
+
+		return null;
 	}
 
 	@Override
 	public boolean shouldFilter() {
 		RequestContext context = RequestContext.getCurrentContext();
 		Object proxy = context.get("proxy");
-	    if (proxy != null && proxy.equals("hello-service")) {
-	        return true;
-	    }
+		if (proxy != null && proxy.equals("hello-service")) {
+			return true;
+		}
 
 		return false;
 	}
